9083f76a3d25c7951e57a8ad12bb876962e951e6
pyk707 <pyk707@hanmail.net>
2021-01-18 22:44:06 +0900

    210118 update

--- crawl_news.py
+++ crawl_news.py
@@ -1,23 +1,19 @@
-import os, re
-import csv
-import requests
+import os , codecs, re, datetime, requests
 import urllib.request as ur
 from bs4 import BeautifulSoup as bs
 
-news = 'https://news.daum.net/'
-#bs(html.read(), 'html.parser')
-soup = bs(ur.urlopen(news).read(), 'html.parser')
+os.chdir(r'C:\Users\user\python')
+url = 'https://news.daum.net/'
+f = open(str(datetime.date.today()) + 'articles.txt','w')
+soup = bs(ur.urlopen(url).read(),'html.parser')
+for i in soup.find_all('div',{"class":"thumb_relate"}):
+    try:
+        f.write(i.text+'\n')
+        f.write(i.find_all('a')[0].get('href') + '\n')
+        soup2 = bs(ur.urlopen(i.find_all('a')[0].get('href')).read(),'html.parser')
 
-soup.find_all('div', {"class" : "relate_thumb"})
-for i in soup.find_all('div', {"class" : "relate_thumb"}):
-    print(i.text)
-
-for i in soup.find_all('a')[:5]:
-    print(i.get('href'))
-
-
-article1 = 'https://news.v.daum.net/v/20210118214828328'
-soup2 = bs(ur.urlopen(article1).read(), 'html.parser')
-
-for i in soup2.find_all('p'):
-    print(i.text)
+        for j in soup2.find_all('p'):
+            f.write(j.text)
+    except:
+        pass
+f.close()
